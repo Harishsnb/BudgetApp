24771d17b281776e27eff81bd4f28a7ac36b80f1
Harish <harishsnb@gmail.com>
2020-08-30 10:32:14 +1200

    Creating an object to store DOMAttributes and receiving the user inputs.

--- Final/app.js
+++ Final/app.js
@@ -6,25 +6,52 @@ var budgetDataController = (function(){
 
 // UI Controller
 var budgetUIController = (function(){
-   //
+    
+    // 1. Get Input values
+    // Creating object and storing the DOM class names. 
+    var DOMAttributes = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'      
+    } ;
+
+    return {
+        getUserInput: function(){
+            return {
+                type: document.querySelector(DOMAttributes.inputType).value, // inc or exp
+                description: document.querySelector(DOMAttributes.inputDescription).value, //user description 
+                value: document.querySelector(DOMAttributes.inputValue).value //get the value entered by user
+            };
+        },
+        getDOMAttributes: function(){
+            return DOMAttributes;
+        }
+
+    }
 })();
 
 // Global app Controller
 
 var budgetController = (function(datactrl, UIctrl){
 
+    var DOM = UIctrl.getDOMAttributes(); // receiving DOMAttributes 
+    
+    console.log(DOM);
     var controlAddItem = function(){
 
         // 1. Get Input values
+        var UserInupt = UIctrl.getUserInput(); //receiving the user input to handle the events
         // 2. Add the new item into budgetDataController
         // 3. Add the new item into the UI
         // 4. Calculate the buget
         // 5. Update the UI to display the budget
-        console.log("Event handling works.")
+        //console.log("Event handling works.")
+        console.log(UserInupt);
     }
     
     //handling the check event when the user clicks the tick icon
-    document.querySelector('.add__btn').addEventListener('click', controlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', controlAddItem);
 
     // user also preses the enter/return key genrally instead of click on tick or submit button.
     //handling the keyboard event of return/enter keys
